4f41a6ee09bad1219c3a9dd5b946bf6f5b88b1d4
ttetrafon <ttetrafon@yahoo.gr>
2026-06-19 19:36:58 +0300

    minor adjustments...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
-
 import cloudflare from "@astrojs/cloudflare";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
